9c044ffcbfcdc2acb959bde4ffe18351088cf4d8
Ernst Blecha <ernst.blecha@gmail.com>
2017-02-09 21:54:56 +0100

    fix #2

--- waitforstring.py
+++ waitforstring.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3 -u
 #using -u here to make stdin unbuffered
 
+# This file is part of gen.sh.
+#
+# gen.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gen.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gen.sh.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 VERSION=1
