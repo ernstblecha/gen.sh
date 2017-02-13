7e8638531d9b1bd76a0d60805514da796ee77dc5
Ernst Blecha <ernst.blecha@gmail.com>
2017-02-13 15:53:12 +0100

    eix-test-obsolete changed (skipped checks...)

--- eix-test-obsolete.sh
+++ eix-test-obsolete.sh
@@ -32,7 +32,6 @@ fi;
 
 generateControlOutput () {
 cat <<EOF
-
 No non-matching entries in /etc/portage/package.keywords
 No non-matching entries in /etc/portage/package.accept_keywords
 No non-matching entries in /etc/portage/package.mask
@@ -43,21 +42,20 @@ No non-matching or empty entries in /etc/portage/package.license
 No non-matching or empty entries in /etc/portage/package.accept_restrict
 No non-matching or empty entries in /etc/portage/package.cflags
 The names of all installed packages are in the database.
-
 No  redundant  entries in /etc/portage/package.{,accept_}keywords
-No uninstalled entries in /etc/portage/package.{,accept_}keywords
+Skipping check: uninstalled entries in /etc/portage/package.{,accept_}keywords
 No  redundant  entries in /etc/portage/package.mask
-No uninstalled entries in /etc/portage/package.mask
+Skipping check: uninstalled entries in /etc/portage/package.mask
 No  redundant  entries in /etc/portage/package.unmask
-No uninstalled entries in /etc/portage/package.unmask
+Skipping check: uninstalled entries in /etc/portage/package.unmask
 Skipping check:  redundant  entries in /etc/portage/package.use
 Skipping check: uninstalled entries in /etc/portage/package.use
 Skipping check:  redundant  entries in /etc/portage/package.env
 Skipping check: uninstalled entries in /etc/portage/package.env
 No  redundant  entries in /etc/portage/package.license
-No uninstalled entries in /etc/portage/package.license
+Skipping check: uninstalled entries in /etc/portage/package.license
 No  redundant  entries in /etc/portage/package.accept_restrict
-No uninstalled entries in /etc/portage/package.accept_restrict
+Skipping check: uninstalled entries in /etc/portage/package.accept_restrict
 Skipping check:  redundant  entries in /etc/portage/package.cflags
 Skipping check: uninstalled entries in /etc/portage/package.cflags
 All installed versions of packages are in the database.
