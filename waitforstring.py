5ef74d1b533554f2d6a222308578854080f2d130
Ernst Blecha <ernst.blecha@gmail.com>
2017-09-27 15:30:59 +0200

    fix all messages from pycodestyle 2.3.1 for waitforstring.py (incrementing version in case something goes horribly wrong)

--- waitforstring.py
+++ waitforstring.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 -u
-#using -u here to make stdin unbuffered
+# using -u here to make stdin unbuffered
 
 # This file is part of gen.sh.
 #
@@ -18,29 +18,28 @@
 
 import sys
 import os
-VERSION=1
+VERSION = 2
 if os.environ.get("GET_VERSION") == "1":
-  print(VERSION)
-  sys.exit(0)
+    print(VERSION)
+    sys.exit(0)
 
-l=0
-n=""
-s=""
-if len(sys.argv) > 1: #get the needle and its length
-    l = len(sys.argv[1])
+w = 0
+n = ""
+s = ""
+if len(sys.argv) > 1:  # get the needle and its length
+    w = len(sys.argv[1])
     n = sys.argv[1]
 
-while l > 0: #"endless" loop if we have a needle
-    c=sys.stdin.read(1)
+while w > 0:  # "endless" loop if we have a needle
+    c = sys.stdin.read(1)
     if len(c) == 0:
-        sys.exit(1) #stream ended, needle not found
-    s+=c
-    s=s[-l:] #store the last l characters for comparison
+        sys.exit(1)  # stream ended, needle not found
+    s += c
+    s = s[-w:]  # store the last l characters for comparison
     if s == n:
-        sys.exit(0) #needle was found
+        sys.exit(0)  # needle was found
 
-#usage message if needle is missing
-import os.path
+# usage message if needle is missing
 print(os.path.basename(sys.argv[0])+" needle")
 print("")
 print("blocks until the string passed in the first argument (\"needle\") is found on stdin or the stream ends")
@@ -54,4 +53,4 @@ print("This message is shown if no string is given")
 print("")
 print("Version of "+os.path.basename(sys.argv[0])+": " + str(VERSION))
 
-sys.exit(2) #errorcode for missing needle
+sys.exit(2)  # errorcode for missing needle
