ebbd38a44dd8f6cb96c843705c0813e64fb499e6
Ernst Blecha <ernst.blecha@gmail.com>
2017-09-27 15:52:21 +0200

    fix all messages from shellcheck 0.4.6 for sudo.sh (incrementing version in case something goes horribly wrong)

--- sudo.sh
+++ sudo.sh
@@ -15,22 +15,22 @@
 # You should have received a copy of the GNU General Public License
 # along with gen.sh.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION=1
+VERSION=2
 if [ "$GET_VERSION" = "1" ]; then
   echo $VERSION
   exit 0
 fi
 
 if [[ $# -eq 0 ]]; then
-  ME=`basename $0`
+  ME=$(basename "$0")
   echo "$ME [commands or sudo-options] - run a command with sudo and show a notification if a password is required"
   echo "Version of $ME: $VERSION"
   sudo -h
   exit 0
 fi;
 
-sudo -n echo -n 2> /dev/null
-if [[ $? != 0 ]]; then
+
+if sudo -n echo -n 2> /dev/null; then
   gen.sh q i "User Interaction Needed (sudo)"
 fi
-sudo $@
+sudo "$@"
